eb59833a6a3a8adaa43f68973670190c65b7a318
yupeng <yupeng@wpdeMacBook-Pro.local>
2026-03-19 18:09:29 +0800

    Add cookiy 1.7.11 artifact

--- Formula/cookiy.rb
+++ Formula/cookiy.rb
@@ -4,7 +4,7 @@ class Cookiy < Formula
   version "1.7.11"
 
   on_arm do
-    url "https://github.com/cookiy-ai/homebrew-tap/releases/download/cookiy-v1.7.11/cookiy-v1.7.11-darwin-arm64.tar.gz"
+    url "https://raw.githubusercontent.com/cookiy-ai/homebrew-tap/main/Artifacts/cookiy-v1.7.11-darwin-arm64.tar.gz"
     sha256 "4710348be0340029fa8f46bb58cfba27e0afdf7c8a8f83584eb0dc8c5e0e5808"
   end
 
